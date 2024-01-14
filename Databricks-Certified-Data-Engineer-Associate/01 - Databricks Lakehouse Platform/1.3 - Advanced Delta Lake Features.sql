dafa6957462ce0428164550da096e4bc5965c06c
tanvir0102 <tanvir0102>
2024-01-14 11:04:32 +0000

    change in Advanced Delta Lake Features

--- Databricks-Certified-Data-Engineer-Associate/01 - Databricks Lakehouse Platform/1.3 - Advanced Delta Lake Features.sql
+++ Databricks-Certified-Data-Engineer-Associate/01 - Databricks Lakehouse Platform/1.3 - Advanced Delta Lake Features.sql
@@ -1,2 +1,78 @@
 -- Databricks notebook source
+DESCRIBE HISTORY employees
+
+-- COMMAND ----------
+
+SELECT * 
+FROM employees VERSION AS OF 1
+
+-- COMMAND ----------
+
+SELECT * 
+FROM employees@v1
+
+-- COMMAND ----------
+
+DELETE FROM employees
+
+-- COMMAND ----------
+
+SELECT * FROM employees
+
+-- COMMAND ----------
+
+RESTORE TABLE employees TO VERSION AS OF 2
+
+-- COMMAND ----------
+
+SELECT * FROM employees
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY employees
+
+-- COMMAND ----------
+
+DESCRIBE DETAIL employees
+
+-- COMMAND ----------
+
+OPTIMIZE employees
+ZORDER BY id
+
+-- COMMAND ----------
+
+DESCRIBE DETAIL employees
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls 'dbfs:/user/hive/warehouse/employees'
+
+-- COMMAND ----------
+
+VACUUM employees
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls 'dbfs:/user/hive/warehouse/employees'
+
+-- COMMAND ----------
+
+VACUUM employees RETAIN 0 HOURS
+
+-- COMMAND ----------
+
+DROP TABLE employees
+
+-- COMMAND ----------
+
+SELECT * FROM employees
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls 'dbfs:/user/hive/warehouse/employees'
+-- MAGIC
+
+-- COMMAND ----------
+
 
