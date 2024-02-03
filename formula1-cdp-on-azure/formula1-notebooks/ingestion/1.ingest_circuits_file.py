cb94fdd53cf7b3432fc7e993bddd342349bcf2bf
tanvir0102 <tanvir0102>
2024-02-03 03:20:17 +0000

    added Ingest circuits file

---
+++ formula1-cdp-on-azure/formula1-notebooks/ingestion/1.ingest_circuits_file.py
@@ -0,0 +1,12 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest circuits.csv file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Read CSV file using the spark dataframe reader
+
+# COMMAND ----------
+
+
