1988d037e9ac4f6cf77b2df7af8f776e0c2a4fd0
tanvir0102 <tanvir0102>
2024-02-03 04:30:23 +0000

    Ingest circuit - completed.

--- formula1-cdp-on-azure/formula1-notebooks/ingestion/1.ingest_circuits_file.py
+++ formula1-cdp-on-azure/formula1-notebooks/ingestion/1.ingest_circuits_file.py
@@ -1,11 +1,87 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ### Ingest circuits.csv file
+# MAGIC ## Ingest circuits.csv file
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##### Step 1 - Read CSV file using the spark dataframe reader
+# MAGIC #### Step 1 - Read CSV file using the spark dataframe reader
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType
+
+# COMMAND ----------
+
+circuits_schema = StructType(fields=[StructField("circuitId", IntegerType(), False),
+                                     StructField("circuitRef", StringType(), True),
+                                     StructField("name", StringType(), True),
+                                     StructField("location", StringType(), True),
+                                     StructField("country", StringType(), True),
+                                     StructField("lat", DoubleType(), True),
+                                     StructField("lag", DoubleType(), True),
+                                     StructField("alt", IntegerType(), True),
+                                     StructField("url", StringType(), True)
+])
+
+# COMMAND ----------
+
+circuits_df = spark.read \
+.option("header",True) \
+.schema(circuits_schema) \
+.csv("/mnt/formula1dl012024/raw/circuits.csv")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 2 - Select only required columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+circuits_selected_df = circuits_df.select(col("circuitId"), col("circuitRef"), col("name"), col("location"), col("country"), col("lat"), col("lag"), col("alt"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 3 - Rename the columns as required
+
+# COMMAND ----------
+
+circuits_renamed_df = circuits_selected_df.withColumnRenamed("circuitId", "circuit_id") \
+    .withColumnRenamed("circuitRef", "circuit_ref") \
+    .withColumnRenamed("lat", "latitude") \
+    .withColumnRenamed("lag", "longitude") \
+    .withColumnRenamed("alt", "altitude")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 4 - Add ingestion date to the dataframe
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+circuits_final_df = circuits_renamed_df.withColumn("ingestion_date", current_timestamp())
+
+# COMMAND ----------
+
+display(circuits_final_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Step 5 - Write data to datalake as parquet
+
+# COMMAND ----------
+
+circuits_final_df.write.mode("overwrite").parquet("/mnt/formula1dl012024/processed/circuits")
 
 # COMMAND ----------
 
