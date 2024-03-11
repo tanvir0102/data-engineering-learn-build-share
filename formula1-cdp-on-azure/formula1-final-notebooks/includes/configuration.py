03920f5f7d8abea5199a796349b5cf6fcc78bf5e
tanvir0102 <tanvir0102>
2024-03-11 01:26:43 +0000

    final changes are done, ingestion notebooks are ready

---
+++ formula1-cdp-on-azure/formula1-final-notebooks/includes/configuration.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+raw_forder_path = '/mnt/formula1dl2024a/raw'
+processed_forlder_path = '/mnt/formula1dl2024a/processed'
+presentation_folder_path = '/mnt/formula1dl2024a/presentation'
+
+# COMMAND ----------
+
+
