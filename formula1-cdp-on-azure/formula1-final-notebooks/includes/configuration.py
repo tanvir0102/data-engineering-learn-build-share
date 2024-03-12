0fa0be3b2429a81e597ba134b5282d828cd1d15d
tanvir0102 <tanvir0102>
2024-03-12 00:29:29 +0000

    added the final notebooks for trans and viz

--- formula1-cdp-on-azure/formula1-final-notebooks/includes/configuration.py
+++ formula1-cdp-on-azure/formula1-final-notebooks/includes/configuration.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 raw_forder_path = '/mnt/formula1dl2024a/raw'
-processed_forlder_path = '/mnt/formula1dl2024a/processed'
+processed_folder_path = '/mnt/formula1dl2024a/processed'
 presentation_folder_path = '/mnt/formula1dl2024a/presentation'
 
 # COMMAND ----------
---
+++ formula1-cdp-on-azure/formula1-final-notebooks/trans/3.constructor_standings.py
@@ -0,0 +1,52 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #### Produce constructor standings
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
+
+# COMMAND ----------
+
+display(race_results_df)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import sum, when, count, col
+
+constructor_standings_df = race_results_df \
+    .groupBy("race_year", "team") \
+    .agg(sum("points").alias("total_points"), count(when(col("position") == 1, True)).alias("wins"))
+
+# COMMAND ----------
+
+display(constructor_standings_df.filter("race_year = 2020"))
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+from pyspark.sql.functions import desc, rank
+
+constructor_rank_spec = Window.partitionBy("race_year").orderBy(desc("total_points"), desc("wins"))
+final_df = constructor_standings_df.withColumn("rank", rank().over(constructor_rank_spec))
+
+# COMMAND ----------
+
+display(final_df.filter("race_year = 2020"))
+
+# COMMAND ----------
+
+final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_presentation.constructor_standings")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_presentation.constructor_standings;
+
+# COMMAND ----------
+
+
